7d3f98e5c1e1c614c78b7669ec8ff04f8ff5a64c
ansehyeong <dkstpgud@gmail.com>
2023-08-22 10:36:00 +0900

    initial commit for DDD model implement

---
+++ src/main/java/com/example/domainmodelpr/DomainModelPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.domainmodelpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainModelPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/domainmodelpr/DomainModelPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domainmodelpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainModelPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
